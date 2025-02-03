2da7c5d70d9e795b8182dc8f98dd1d4443b3b1ab
MayankTripathi033 <mayank033023@gmail.com>
2025-02-03 15:17:47 +0530

    Fixed Problems

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,58 @@
-import React, {useEffect, useState} from 'react'
-import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
-import {useSelector} from "react-redux";
-
-
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/Config.js";
+import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
 
 function Home() {
-    const status = useSelector((state) => state.auth.status);
-    console.log(status);
-    const [posts, setPosts] = useState([])
+  const status = useSelector((state) => state.auth.status);
+  console.log(status);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
 
-    // console.log(posts[0].status);
+  // console.log(posts[0].status);
 
-  
-    if (posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
-    }
-    if(status === true){
+  if (posts.length === 0) {
     return (
-        <div className='w-full py-8'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
-            </Container>
-        </div>
-    )
-}
-return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  if (status === true) {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  return (
     <>
-        <h1>Please Login To see Post</h1>
+      <h1>Please Login To see Post</h1>
     </>
-)
+  );
 }
 
-export default Home
+export default Home;
